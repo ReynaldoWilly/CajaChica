532c0a7c5305e5b407804dc6934122b30b800847
Reynaldo Rios <reynaldowilly@gmail.com>
2019-01-23 00:42:19 -0400

    INICIO DEL PROYECTO

---
+++ src/main/java/com/allsoft/cajachicav1/Principal.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.allsoft.cajachicav1;
+
+import com.allsoft.view.vtnLogin;
+
+/**
+ *
+ * @author Reynaldo
+ */
+public class Principal {
+
+    public static void main(String arg[]) {
+        System.out.println("com.allsoft.cajachicav1.Principal.main()");
+        vtnLogin login=new vtnLogin();
+        login.show();
+    }
+}
